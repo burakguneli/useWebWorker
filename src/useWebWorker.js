148390011cdf26888180e15035ce26da20097b65
burak.gueneli <burak.gueneli@uberall.com>
2024-01-24 17:19:45 +0100

    simplify useWebWorker hook

--- src/useWebWorker.js
+++ src/useWebWorker.js
@@ -3,52 +3,36 @@ import { useEffect, useRef } from 'react';
 /**
  * Custom hook that creates a Web Worker and handles communication with it.
  *
- * @param {function} workerFunction - The function to be executed by the Web Worker.
+ * @param {function} functionToBeExecutedByWorker - The function to be executed by the Web Worker.
  * @param {function} callbackFunction - The function to be called with the result from the Web Worker.
  * @returns {function} - A function to post a message to the Web Worker.
  */
-const useWebWorker = (workerFunction, callbackFunction) => {
+const useWebWorker = (functionToBeExecutedByWorker, callbackFunction) => {
   const workerRef = useRef();
 
-  useEffect(() => {
-    // Create a new Web Worker from the provided function
-    const createWorker = () => {
-      const blob = new Blob([`self.onmessage = ${workerFunction}`], {
-        type: 'application/javascript',
-      });
+  // Create a new Web Worker from the provided function
+  const blob = new Blob([`self.onmessage = ${functionToBeExecutedByWorker}`], {
+    type: 'application/javascript',
+  });
 
-      const worker = new Worker(URL.createObjectURL(blob));
-      workerRef.current = worker;
+  const worker = new Worker(URL.createObjectURL(blob));
+  workerRef.current = worker;
 
-      // Handle messages from the worker
-      worker.onmessage = (event) => {
-        // Handle the result sent by the worker
-        console.log('Result from worker:', event.data);
+  // Handle messages from the worker
+  worker.onmessage = (event) => {
+    // Handle the result sent by the worker
+    callbackFunction(event.data)
+  };
 
-        callbackFunction(event.data)
-      };
-
-      // Handle errors from the worker
-      worker.onerror = (error) => {
-        console.error('Error in worker:', error);
-      };
-    };
-
-    createWorker();
-
-    return () => {
-      // Terminate the worker when the component unmounts
-      if (workerRef.current) {
-        workerRef.current.terminate();
-      }
-    };
-  }, [callbackFunction, workerFunction]);
+  // Handle errors from the worker
+  worker.onerror = (error) => {
+    console.error('Error in worker:', error);
+  };
 
   // Function to post a message to the worker
   const postMessageToWorker = (message) => {
-    if (workerRef.current) {
-
-      workerRef.current.postMessage(message);
+    if (worker) {
+      worker.postMessage(message);
     }
   };
 
